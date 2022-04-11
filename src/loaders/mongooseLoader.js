0b04104e0288e2264a3cd4de9cf1a319ebce5843
daylimbic <sidhys1@gmail.com>
2022-04-11 17:40:41 -0400

    Added error handling for mongoose.connect
    Without a ".catch" statement, and on a connection error, the API would simply print nothing, rather than provide valid debug information.

--- src/loaders/mongooseLoader.js
+++ src/loaders/mongooseLoader.js
@@ -4,7 +4,7 @@ const HOST_NAME = 'localhost';
 const DATABASE_NAME = 'Database';
 
 function load() {
-  mongoose.connect('mongodb://' + HOST_NAME + '/' + DATABASE_NAME);
+  mongoose.connect('mongodb://' + HOST_NAME + '/' + DATABASE_NAME).catch(error => console.error(error));
   mongoose.connection.on('connect', () => console.log('MongoDB connected'));
 }
 
