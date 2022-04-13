3a41d35ba6f77d0bc701a08dbb6ae1f4b44fdc67
Omer Sabic <osabic2004@gmail.com>
2022-04-13 14:05:26 +0200

    moved the hostname and database name to .env for better local testing

--- src/loaders/mongooseLoader.js
+++ src/loaders/mongooseLoader.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-const HOST_NAME = 'localhost';
-const DATABASE_NAME = 'Database';
-
 function load() {
-  mongoose.connect('mongodb://' + HOST_NAME + '/' + DATABASE_NAME);
+  mongoose.connect('mongodb://' + process.env.HOST_NAME + '/' + process.env.DATABASE_NAME);
   mongoose.connection.on('connect', () => console.log('MongoDB connected'));
 }
 
