d44f4c18be5df0cb1b5ce41e453732e2ce60c7c2
Omer Sabic <osabic2004@gmail.com>
2022-04-13 12:49:27 +0200

    Added validation for the /skills POST request

--- src/api/controllers/skill.controller.js
+++ src/api/controllers/skill.controller.js
@@ -12,16 +12,22 @@ class SkillController {
 
   createSkill(req, res) {
     console.log('POST /skills');
-
     const skill = new Skill(req.body);
-    skill.save();
 
-    res.status(201).json(skill);
+    skill.validate(err => {
+      if (err) return res.status(400).json({ errCode: 400, message: "Validation failed. Please check your input." });
+
+      skill.save();
+
+      return res.status(201).json(skill);
+    })
+
+
   }
 
-  editSkill(req, res) {}
+  editSkill(req, res) { }
 
-  deleteSkill(req, res) {}
+  deleteSkill(req, res) { }
 }
 
 module.exports = new SkillController();
