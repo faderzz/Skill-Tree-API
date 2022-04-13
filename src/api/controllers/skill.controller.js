c2dd1f0fed014a8ed6f356018c8dc6afc6aeadb3
Omer Sabic <osabic2004@gmail.com>
2022-04-13 13:19:44 +0200

    Duplicate field checking

--- src/api/controllers/skill.controller.js
+++ src/api/controllers/skill.controller.js
@@ -14,8 +14,10 @@ class SkillController {
     console.log('POST /skills');
     const skill = new Skill(req.body);
 
-    skill.validate(err => {
-      if (err) return res.status(400).json({ errCode: 400, message: "Validation failed. Please check your input." });
+    skill.validate(async err => {
+      if (err) return res.status(400).json({ errCode: 400, message: "Validation failed. Please check your input.", error: err });
+
+      if (await Skill.findOne({ iconName: skill.iconName }).exec()) return res.status(409).json({ errCode: 409, message: "Duplicate input, please check that the \"iconName\" field is not the same as any other document." });
 
       skill.save();
 
