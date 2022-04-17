93a97370bdca7cc9dd36b69ba8670dcdb8b3adf1
Swatarianess <swatarianess@gmail.com>
2022-04-17 13:52:24 +0200

    format cleanup

--- src/api/routes/v1/skill.routes.js
+++ src/api/routes/v1/skill.routes.js
@@ -4,7 +4,6 @@ const skillController = require('../../controllers/skill.controller');
 const router = express.Router();
 const prefix = "/v1/skills"
 
-
 router.get(prefix + '/', skillController.getSkills);
 router.post(prefix + '/', skillController.createSkill);
 router.put(prefix + '/:id', skillController.editSkill);
