2a034e01a371641f4b89a3cdbd0a9ad2de93bbaa
Vinicius <viniciusantonio54@gmail.com>
2022-04-21 21:14:59 +0100

    Fix style problems

--- src/api/controllers/skill.controller.js
+++ src/api/controllers/skill.controller.js
@@ -5,7 +5,7 @@ class SkillController {
     console.log('GET /skills');
 
     const skills = await Skill.find({});
-    console.log(skills)
+    console.log(skills);
 
     res.status(200).json(skills);
   }
@@ -15,21 +15,18 @@ class SkillController {
     const skill = new Skill(req.body);
 
     skill.validate(async err => {
-      if (err) return res.status(400).json({ errCode: 400, message: "Validation failed. Please check your input.", error: err });
+      if (err) return res.status(400).json({ errCode: 400, message: 'Validation failed. Please check your input.', error: err });
       
-      if(await Skill.findOne({ title: skill.title, level: skill.level }).exec()) return res.status(409).json({ errCode: 409, message: "Skill already exists." });
+      if (await Skill.findOne({ title: skill.title, level: skill.level }).exec()) return res.status(409).json({ errCode: 409, message: 'Skill already exists.' });
 
       skill.save();
 
       return res.status(201).json(skill);
-    })
-
-
+    });
   }
 
-  editSkill(req, res) { }
-
-  deleteSkill(req, res) { }
+  // editSkill(req, res) { }
+  // deleteSkill(req, res) { }
 }
 
 module.exports = new SkillController();
