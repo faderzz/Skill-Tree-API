4a1f67c79463320d3a45bd729782806c13c174bc
Sean Hardy <seanjhardy@icloud.com>
2022-06-09 06:26:50 +0100

    Update mongooseLoader.js

--- src/loaders/mongooseLoader.js
+++ src/loaders/mongooseLoader.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 function load() {
-  mongoose.connect(process.env.DB_KEY).catch(error => console.error(error));
+  mongoose.connect(process.env.DB_KEY, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+    if (err){
+      log.warn("Error",err)
+    } else{
+      log.info("Connected to db successfully")
+    }
+  });
   //Idk what this does, but it doesn't print anything
   mongoose.connection.on("connect", () => console.log("MongoDB connected"));
 }
