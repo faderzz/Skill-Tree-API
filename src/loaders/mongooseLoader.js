ace271fffb793e8efbab00074a5839725db0b87d
Sean Hardy <seanjhardy@icloud.com>
2022-06-09 06:31:06 +0100

    Update mongooseLoader.js

--- src/loaders/mongooseLoader.js
+++ src/loaders/mongooseLoader.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 
 function load() {
   mongoose.connect(process.env.DB_KEY, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
-    if (err){
-      log.warn("Error",err)
-    } else{
-      log.info("Connected to db successfully")
+    if (err) {
+      console.log("Error: ",err);
+    } else {
+      console.log("Connected to db successfully");
     }
   });
   //Idk what this does, but it doesn't print anything
