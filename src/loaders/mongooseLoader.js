55e8bd1ad8701c9b3391df50e99a8fd4229ac66a
Steve <swatarianess@users.noreply.github.com>
2022-06-09 11:05:35 +0200

    Update mongooseLoader.js
    Placing the `mongoose.connection.on` to have the logic before actually trying to connect. 😄

--- src/loaders/mongooseLoader.js
+++ src/loaders/mongooseLoader.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 function load() {
+  mongoose.connection.on("connect", () => console.log("MongoDB connected"));
+
   mongoose.connect(process.env.DB_KEY, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
     if (err) {
       console.log("Error: ",err);
@@ -9,8 +11,6 @@ function load() {
       console.log("Connected to db successfully");
     }
   });
-  //Idk what this does, but it doesn't print anything
-  mongoose.connection.on("connect", () => console.log("MongoDB connected"));
 }
 
 module.exports = load;
