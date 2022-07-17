79b1dd013fa9c6e2af40dd39ea487d0c068494cc
vlyr <vlyr@tuta.io>
2022-07-17 14:01:43 +0300

    revert accidental merge

--- src/api/routes/v1/routes.js
+++ src/api/routes/v1/routes.js
@@ -6,5 +6,4 @@ const router = express.Router();
 
 router.get("/getNodes", auth, controller.getNodes);
 router.get("/inList", auth, controller.inList);
-router.get("/status", controller.getStatus);
 module.exports = router;
